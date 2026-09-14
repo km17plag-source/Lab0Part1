461a29f928817de6e57b09ccf95758875a51bdba
km17plag <km17plag@siena.edu>
2026-09-14 13:27:49 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,15 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Kyra Plagainos
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("I'm from Pearl River, New York");
+        System.out.println("My favorite restaurant there is Bella's Kitchen");
+        System.out.println("My favorite menu item is the crispy chicken wrap and fresh hummus");
     }
 }
